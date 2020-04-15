50699763a4cf312c0d0b2cfb8d5eb47d68a7c16f
wanglei <wl13773708199@163.com>
2020-04-15 17:27:42 +0800

    用户权限

--- study1/app01/urls.py
+++ study1/app01/urls.py
@@ -3,7 +3,11 @@ from django.conf.urls import url
 from app01 import views
 
 urlpatterns=[
-    url('^index/',views.index),
-    url('^student/',views.StudentsView.as_view()),
-    url('^order/',views.OrderView.as_view()),
+    # url('^index/',views.index),
+    # url('^student/',views.StudentsView.as_view()),
+    # url('^order/',views.OrderView.as_view()),
+
+
+    url('^dog/',views.DogView.as_view())
+
 ]
--- study1/app01/views.py
+++ study1/app01/views.py
@@ -11,65 +11,97 @@ from django.views.decorators.csrf import csrf_exempt, csrf_protect
 
 # @csrf_exempt
 # @csrf_protect
-def index(request):
+# def index(request):
+#
+#     user_list = ['hello','world']
+#
+#
+#     return HttpResponse(json.dumps(user_list))
+#
+#
+# class MyBaseView(object):
+#
+#     # todo 可以起到装饰器的作用
+#     @method_decorator(csrf_exempt)
+#     def dispatch(self,request,*args,**kwargs):
+#
+#         print('before')
+#         ret = super(MyBaseView,self).dispatch(request,*args,**kwargs)
+#         print('after')
+#         return ret
+#
+#
+# class StudentsView(MyBaseView,View):
+#
+#     @method_decorator(csrf_protect)
+#     def dispatch(self,request,*args,**kwargs):
+#         ret = super(StudentsView, self).dispatch(request,*args,**kwargs)
+#         return ret
+#
+#
+#     def get(self,request,*args,**kwargs):
+#         return HttpResponse('GET')
+#
+#
+#     def post(self,request,*args,**kwargs):
+#         return HttpResponse('POST')
+#
+#     def put(self,request,*args,**kwargs):
+#         return HttpResponse('PUT')
+#
+#     def delete(self,request,*args,**kwargs):
+#         return HttpResponse('DELETE')
+#
+#
+# class OrderView(View):
+#
+#     def get(self,request,*args,**kwargs):
+#
+#         ret = {
+#             'code':1000,
+#             'msg':'xxxx'
+#         }
+#
+#         return HttpResponse(json.dumps(ret),status=201)
+#
+#     def post(self,request,*args,**kwargs):
+#         return HttpResponse('创建订单')
+#
+#     def put(self,request,*args,**kwargs):
+#         return HttpResponse('修改订单')
+#
+#     def delete(self,request,*args,**kwargs):
+#         return HttpResponse('删除订单')
 
-    user_list = ['hello','world']
+################################################
+from rest_framework.views import APIView
+from rest_framework.authentication import BasicAuthentication
+from rest_framework import exceptions
+
+class MyAuthentication(object):
+    def authenticate(self,request):
+        token = request._request.GET.get('token')
+        if not token:
+            raise exceptions.AuthenticationFailed('用户认证失败')
+
+        return ('alex','123')
+
+    def authenticate_header(self,request):
+        pass
 
 
-    return HttpResponse(json.dumps(user_list))
 
+class DogView(APIView):
 
-class MyBaseView(object):
+    authentication_classes = [MyAuthentication]
+    def get(self,request):
+        return HttpResponse('获取')
 
-    # todo 可以起到装饰器的作用
-    @method_decorator(csrf_exempt)
-    def dispatch(self,request,*args,**kwargs):
+    def post(self,request):
+        return HttpResponse('创建')
 
-        print('before')
-        ret = super(MyBaseView,self).dispatch(request,*args,**kwargs)
-        print('after')
-        return ret
+    def put(self,request):
+        return HttpResponse('修改')
 
-
-class StudentsView(MyBaseView,View):
-
-    @method_decorator(csrf_protect)
-    def dispatch(self,request,*args,**kwargs):
-        ret = super(StudentsView, self).dispatch(request,*args,**kwargs)
-        return ret
-
-
-    def get(self,request,*args,**kwargs):
-        return HttpResponse('GET')
-
-
-    def post(self,request,*args,**kwargs):
-        return HttpResponse('POST')
-
-    def put(self,request,*args,**kwargs):
-        return HttpResponse('PUT')
-
-    def delete(self,request,*args,**kwargs):
-        return HttpResponse('DELETE')
-
-
-class OrderView(View):
-
-    def get(self,request,*args,**kwargs):
-
-        ret = {
-            'code':1000,
-            'msg':'xxxx'
-        }
-
-        return HttpResponse(json.dumps(ret),status=201)
-
-    def post(self,request,*args,**kwargs):
-        return HttpResponse('创建订单')
-
-    def put(self,request,*args,**kwargs):
-        return HttpResponse('修改订单')
-
-    def delete(self,request,*args,**kwargs):
-        return HttpResponse('删除订单')
-
+    def delete(self,request):
+        return HttpResponse('删除')
